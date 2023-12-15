4d86146e74d5a3ce876347013260936c48470287
wigor <wigor21@hotmail.com>
2023-12-14 23:57:34 -0300

    feat: Add dark and light theme colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
+import { ThemeProvider } from "styled-components";
+
 import Layout from "./components/Layout";
 import GlobalStyles from "./styles/GlobalStyles";
+import dark from "./styles/themes/dark";
 
 const App = () => {
   return (
-    <>
+    <ThemeProvider theme={dark}>
       <GlobalStyles />
       <Layout />
-    </>
+    </ThemeProvider>
   );
 };
 
