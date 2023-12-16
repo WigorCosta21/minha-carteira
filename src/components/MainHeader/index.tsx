30bd560f27973cbb49e1ec2f145fb3067426f9cb
wigor <wigor21@hotmail.com>
2023-12-16 07:40:23 -0300

    Add Main Header component

--- src/components/MainHeader/index.tsx
+++ src/components/MainHeader/index.tsx
@@ -1,9 +1,23 @@
+import { useMemo } from "react";
+
 import * as S from "./styles";
+import emojis from "../../utils/emojis";
 
 const MainHeader = () => {
+  const emoji = useMemo(() => {
+    const indice = Math.floor(Math.random() * emojis.length);
+
+    return emojis[indice];
+  }, []);
+
   return (
     <S.Container>
-      <h1>MainHeader</h1>
+      <h1>Toogle</h1>
+
+      <S.Profile>
+        <S.Welcome>Olá, {emoji}</S.Welcome>
+        <S.UserName>Wigor Ribeiro</S.UserName>
+      </S.Profile>
     </S.Container>
   );
 };
