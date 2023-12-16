116398386c4cce09bfc9336c60ff0dbaf5a54620
wigor <wigor21@hotmail.com>
2023-12-16 19:11:02 -0300

    feat: Add Aside component and install react-icons

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -1,9 +1,39 @@
+import {
+  MdDashboard,
+  MdArrowDownward,
+  MdArrowUpward,
+  MdExitToApp,
+} from "react-icons/md";
+
+import logoImg from "../../assets/logo.svg";
+
 import * as S from "./styles";
 
 const Aside = () => {
   return (
     <S.Container>
-      <h1>Aside</h1>
+      <S.Header>
+        <S.LogoImg src={logoImg} alt="Logo Minha Carteira" />
+        <S.Title>Minha Carteira</S.Title>
+      </S.Header>
+      <S.MenuContainer>
+        <S.MenuItemLink href="#">
+          <MdDashboard />
+          Dashboard
+        </S.MenuItemLink>
+        <S.MenuItemLink href="#">
+          <MdArrowUpward />
+          Entradas
+        </S.MenuItemLink>
+        <S.MenuItemLink href="#">
+          <MdArrowDownward />
+          Saídas
+        </S.MenuItemLink>
+        <S.MenuItemLink href="#">
+          <MdExitToApp />
+          Sair
+        </S.MenuItemLink>
+      </S.MenuContainer>
     </S.Container>
   );
 };
