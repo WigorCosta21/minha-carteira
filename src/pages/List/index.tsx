a92a0552005575dc3601523a54ba98bfc65ed82d
wigor <wigor21@hotmail.com>
2023-12-17 13:10:34 -0300

    feat: Add filter button

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -4,17 +4,33 @@ import SelectInput from "../../components/SelectIput";
 import * as S from "./styles";
 
 const List = () => {
-  const options = [
-    { value: "Rodrigo", label: "Rodrigo" },
-    { value: "Maria", label: "Maria" },
-    { value: "Ana", label: "Ana" },
+  const months = [
+    { value: 1, label: "Janeiro" },
+    { value: 2, label: "Fevereiro" },
+    { value: 3, label: "Março" },
+  ];
+
+  const years = [
+    { value: 2024, label: 2024 },
+    { value: 2025, label: 2025 },
+    { value: 2026, label: 2026 },
   ];
 
   return (
     <S.Container>
       <ContentHeader title="Saídas" linecolor="#E44C4E">
-        <SelectInput options={options} />
+        <SelectInput options={months} />
+        <SelectInput options={years} />
       </ContentHeader>
+
+      <S.Filters>
+        <button type="button" className="tag-filter tag-filter-recurrent">
+          Recorrentes
+        </button>
+        <button type="button" className="tag-filter tag-filter-eventual">
+          Eventuais
+        </button>
+      </S.Filters>
 
       <S.Content>
         <HistoryFinanceCard
